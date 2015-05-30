cfaad6c52e3145e4b28807bc85b33d74f0451543
Itachihi <1327690968@qq.com>
2015-05-30 09:59:54 +0800

    Update temperature.h

--- temperature.h
+++ temperature.h
@@ -1,8 +1,7 @@
 #ifndef __TEMPERATURE_H__
 #define __TEMPERATURE_H__
 
-#define I2C1_SLAVE_ADDRESS  0x48
-#define I2C_SPEED           10000
+#define LM75_ADDRESS  0x48
 #define LM75_TEMP_REG       0x00
 
 #endif

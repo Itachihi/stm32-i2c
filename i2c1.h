6ed9fce62bfece9701ceeb5e65b3966d15bf32b5
Itachihi <1327690968@qq.com>
2015-05-30 11:33:42 +0800

    Update i2c1.h

--- i2c1.h
+++ i2c1.h
@@ -3,10 +3,6 @@
 
 #include <stdint.h>
 
-#define  GPIOn    GPIOB
-#define  SDA_PIN  GPIO_Pin_7
-#define  SCL_PIN  GPIO_Pin_6
-
 int I2C1_Write(int8_t addr, const uint8_t *dat, int len);
 int I2C1_Read(uint8_t addr, uint8_t *dat, int len);
 
